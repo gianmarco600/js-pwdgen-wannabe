fb56b059852e275bfaf56fa75fc0752606f4bbae
gianmarco <gianmarco.palombo600@gmail.com>
2021-05-26 16:44:53 +0200

    add algoritmo

--- js/style.js
+++ js/style.js
@@ -1,2 +1,87 @@
 
-var nome = prompt("Enter your name");
+var nome , cognome , colore;
+
+nome = prompt("Inserire nome");
+nome = nome.toLowerCase();
+console.log(nome);
+
+cognome = prompt("Inserire cognome");
+cognome = cognome.toLowerCase();
+console.log(cognome);
+
+colore = prompt("Inserire colore preferito");
+colore = colore.toLowerCase();
+console.log(colore);
+
+var pwd;
+
+
+
+
+var nomeLen = nome.length;
+var i;
+var arrNome = [];
+var pwd = [];
+nome = nome.trim();
+
+for (i = 0; i < nomeLen; i++) {
+    arrNome = nome.split("");
+    var x = arrNome[i];
+
+
+    if (i == 0 || i % 2 == 0){
+        x = x.toUpperCase();
+    }
+    pwd.push(x);
+    console.log(pwd);
+  } 
+
+console.log(pwd);
+
+// ---------------------------------------------------------------
+
+var cognomeLen = cognome.length;
+var i;
+var arrCognome = [];
+cognome = cognome.trim();
+
+for (i = 0; i < cognomeLen; i++) {
+    arrCognome = cognome.split("");
+    var x = arrCognome[i];
+
+
+    if (i == 0 || i % 2 == 0){
+        x = x.toUpperCase();
+    }
+    pwd.push(x);
+    console.log(pwd);
+  } 
+
+console.log(pwd);
+
+
+
+// -----------------------------------------------
+
+var coloreLen = colore.length;
+var i;
+var arrColore = [];
+colore = colore.trim();
+
+for (i = 0; i < coloreLen; i++) {
+    arrColore = colore.split("");
+    var x = arrColore[i];
+
+
+    if (i == 0 || i % 2 == 0){
+        x = x.toUpperCase();
+    }
+    pwd.push(x);
+    console.log(pwd);
+  } 
+
+console.log(pwd);
+
+
+var password = pwd.toString();
+console.log(password);
